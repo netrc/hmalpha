beb3488e51cea5273a9c83993068dea9d056e71a
richard <ric@netrc.com>
2014-07-18 02:11:01 +0000

    converted to express

--- hmAlpha.js
+++ hmAlpha.js
@@ -1,17 +1,27 @@
 
-var http = require('http');
-var path = require('path');
+//var http = require('http');
+//var path = require('path');
 var express = require('express');
-var router = express();
+//var router = express();
 
 
-var server = http.createServer(router);
+//var server = http.createServer(router);
 
 
-router.use(express.static(path.resolve(__dirname, 'client')));
+//router.use(express.static(path.resolve(__dirname, 'client')));
 
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.get('/', function (req, res) {
+  res.render('index',
+  { title : 'Home' }
+  );
 });
+
+app.listen(process.env.PORT, process.env.IP);
+console.log("express hmalpha running");
+//server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+//  var addr = server.address();
+//  console.log("Chat server listening at", addr.address + ":" + addr.port);
+//});
